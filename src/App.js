bee8d747064cd8e32b07c6ab15d78889f722303d
Carterahart18 <carterahart@gmail.com>
2018-08-26 14:54:16 -0700

    Set up gh-pages deployment commands

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          This app is meant to play Conway's Game of Life!
+        </p>
       </div>
     );
   }
