999a5edcb75dd7cc03cf00718443635fb4791837
Carterahart18 <carterahart@gmail.com>
2019-07-27 19:41:55 -0700

    Updates

---
+++ precache-manifest.b394954053b01c60c6b66ced4eb5f2ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d37d28db46125831ad384cf06107bf0",
+    "url": "/gameoflife/index.html"
+  },
+  {
+    "revision": "237dbb7308fe525713a7",
+    "url": "/gameoflife/static/css/main.78c78f8d.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/gameoflife/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "237dbb7308fe525713a7",
+    "url": "/gameoflife/static/js/main.fc86c0cb.chunk.js"
+  },
+  {
+    "revision": "134693c1149dfffb8647",
+    "url": "/gameoflife/static/js/runtime~main.72a08090.js"
+  }
+]);
